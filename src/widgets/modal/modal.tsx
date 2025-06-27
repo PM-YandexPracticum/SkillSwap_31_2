799c3cc2845cddb9d3db32bc653a331c5f8f8f2c
Daniil Sukhov <suxov.daniilll@mail.ru>
2025-06-27 14:21:23 +0300

    fix: Modal header. Closes#29

--- src/widgets/modal/modal.tsx
+++ src/widgets/modal/modal.tsx
@@ -6,26 +6,22 @@ import { TModalUIProps } from '@app/shared/ui/modal/type';
 
 const modalRoot = document.getElementById('modals');
 
-export const Modal: FC<TModalUIProps> = memo(
-  ({ title, onClose, isHeader = true, children }) => {
-    useEffect(() => {
-      const handleEsc = (e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-          onClose();
-        }
-      };
+export const Modal: FC<TModalUIProps> = memo(({ onClose, children }) => {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
 
-      document.addEventListener('keydown', handleEsc);
-      return () => {
-        document.removeEventListener('keydown', handleEsc);
-      };
-    }, [onClose]);
+    document.addEventListener('keydown', handleEsc);
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [onClose]);
 
-    return ReactDOM.createPortal(
-      <ModalUI title={title} onClose={onClose} isHeader={isHeader}>
-        {children}
-      </ModalUI>,
-      modalRoot as HTMLDivElement
-    );
-  }
-);
+  return ReactDOM.createPortal(
+    <ModalUI onClose={onClose}>{children}</ModalUI>,
+    modalRoot as HTMLDivElement
+  );
+});
