811613fefd6b559d5af5617480dec9c0dcc72b21
Izrekatel <gelrudboris@gmail.com>
2025-07-03 15:04:51 +0500

    ready

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,28 @@
 import styles from './home.module.scss';
+import { useState } from 'react';
 
 import { Aside } from '@ui/aside/aside';
 import { SkillsList } from '@pages/skills-list';
 import { skillListTypes } from '@lib/constants';
 
 export const Home = () => {
-  const filtred: boolean = true;
+  const [filtered, setFiltered] = useState<boolean>(false);
 
   return (
     <div className={styles.homePage}>
       <main className={styles.content}>
         <Aside />
-        if (filtred) {
+        
+        {/* Выдаем страницу подходящих предложений, если есть фильтр */}
+        {filtered ? (
           <SkillsList type={skillListTypes.appropriate} />
-        }
-        else {<div className={styles.resultsSection}>
-          <SkillsList type={skillListTypes.popular} />
-          <SkillsList type={skillListTypes.new} />
-          <SkillsList type={skillListTypes.recommended} />
-        </div>}
+        ) : (
+          <div className={styles.resultsSection}>
+            <SkillsList type={skillListTypes.popular} />
+            <SkillsList type={skillListTypes.new} />
+            <SkillsList type={skillListTypes.recommended} />
+          </div>
+        )}
       </main>
     </div>
   );
